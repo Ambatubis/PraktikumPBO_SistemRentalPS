caf6ab60d45ff5700e1cb12c6157871bfca7142f
Bisma Putra Pangestu <bismapangestu02@gmail.com>
2026-05-31 17:20:30 +0700

    Add files via upload

---
+++ db_rental_ps.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 31, 2026 at 12:20 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_rental_ps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_detail` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_ps` int(11) NOT NULL,
+  `id_paket` int(11) NOT NULL,
+  `lama_main` int(11) NOT NULL,
+  `jam_mulai` datetime DEFAULT NULL,
+  `jam_selesai` datetime DEFAULT NULL,
+  `subtotal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(11) NOT NULL,
+  `nama_member` varchar(100) NOT NULL,
+  `no_hp` varchar(20) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `status_member` enum('Regular','VIP') DEFAULT 'Regular'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id_member`, `nama_member`, `no_hp`, `alamat`, `status_member`) VALUES
+(1, 'Andi Saputra', '08123456789', 'Yogyakarta', 'VIP'),
+(2, 'Budi Santoso', '08234567891', 'Sleman', 'Regular');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paket_rental`
+--
+
+CREATE TABLE `paket_rental` (
+  `id_paket` int(11) NOT NULL,
+  `nama_paket` varchar(50) NOT NULL,
+  `durasi_jam` int(11) NOT NULL,
+  `harga_paket` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `paket_rental`
+--
+
+INSERT INTO `paket_rental` (`id_paket`, `nama_paket`, `durasi_jam`, `harga_paket`) VALUES
+(1, 'Paket 1 Jam', 1, 8000),
+(2, 'Paket 2 Jam', 2, 15000),
+(3, 'Paket 3 Jam', 3, 22000),
+(4, 'Paket Malam', 6, 40000),
+(5, 'Paket 4 Jam', 4, 28000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `metode_bayar` enum('Cash','QRIS','Debit') NOT NULL,
+  `jumlah_bayar` int(11) NOT NULL,
+  `kembalian` int(11) NOT NULL,
+  `tanggal_bayar` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playstation`
+--
+
+CREATE TABLE `playstation` (
+  `id_ps` int(11) NOT NULL,
+  `nama_ps` varchar(50) NOT NULL,
+  `tipe_ps` varchar(20) NOT NULL,
+  `status_ps` enum('Tersedia','Dipakai') DEFAULT 'Tersedia',
+  `harga_per_jam` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `playstation`
+--
+
+INSERT INTO `playstation` (`id_ps`, `nama_ps`, `tipe_ps`, `status_ps`, `harga_per_jam`) VALUES
+(1, 'PS A', 'PS4', 'Tersedia', 8000),
+(2, 'PS B', 'PS4', 'Tersedia', 8000),
+(3, 'PS C', 'PS5', 'Tersedia', 12000),
+(4, 'PS D', 'PS5', 'Tersedia', 12000),
+(5, 'PS E', 'PS3', 'Tersedia', 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `tanggal` datetime DEFAULT current_timestamp(),
+  `id_member` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `total_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `role` enum('admin','kasir') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `role`) VALUES
+(1, 'admin', 'admin123', 'Administrator', 'admin'),
+(2, 'kasir', 'kasir123', 'Kasir Rental', 'kasir');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_ps` (`id_ps`),
+  ADD KEY `id_paket` (`id_paket`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `paket_rental`
+--
+ALTER TABLE `paket_rental`
+  ADD PRIMARY KEY (`id_paket`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_transaksi` (`id_transaksi`);
+
+--
+-- Indexes for table `playstation`
+--
+ALTER TABLE `playstation`
+  ADD PRIMARY KEY (`id_ps`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_member` (`id_member`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `paket_rental`
+--
+ALTER TABLE `paket_rental`
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `playstation`
+--
+ALTER TABLE `playstation`
+  MODIFY `id_ps` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_ps`) REFERENCES `playstation` (`id_ps`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_transaksi_ibfk_3` FOREIGN KEY (`id_paket`) REFERENCES `paket_rental` (`id_paket`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_member`) REFERENCES `member` (`id_member`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
